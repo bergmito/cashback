b7e4de7661874a2eff628e409b1917f59401545a
bergmito <bergmansfelipe@gmail.com>
2020-02-11 02:08:26 -0300

    Fixed setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from models.revendedor import Revendedor
 from models.compra import Compra
 
 engine = create_engine('mysql+pymysql://root:hinade2019@localhost')
-engine.execute('DROP DATABASE IF EXISTS cashback')
+engine.execute('DROP DATABASE IF EXISTS `cashback`')
 engine.execute('CREATE DATABASE IF NOT EXISTS `{0}`'.format('cashback'))
 engine.execute('USE `cashback`')
 Revendedor.metadata.create_all(engine)
